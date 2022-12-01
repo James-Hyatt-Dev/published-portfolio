61b1a2bfe96b60d6cc0e90a55ecb5d6c323cbbe0
James Hyatt <76410700+James-Hyatt-Dev@users.noreply.github.com>
2022-12-01 13:31:23 -0500

    adds Header and css

--- components/Header.js
+++ components/Header.js
@@ -8,31 +8,45 @@ function Header() {
   return (
     <div className="w-full pb-36">
         {/* Logo */}
-        <div className="flex border w-full py-2 fixed top-0 bg-black bg-opacity-90">
-            <div className="flex mx-auto">
+        <div className="flex w-full py-2 fixed top-0 bg-black bg-opacity-90 items-center sm:justify-around">
+            <div className="flex mx-auto sm:w-1/4">
                 <Image width={125} src={Logo} alt='james hyatt personal logo containing a single jay and a single h' />
+            </div>
+            <div className="hidden sm:inline sm:w-3/4">
+                <div className="flex justify-around">
+                    <Link href='/'>Home</Link>
+                    <Link href='/about'>About</Link>
+                    <Link href='/resume'>Resume</Link>
+                    <Link href='/work'>Work</Link>
+                </div>
+                <div className="justify-end flex gap-2 pr-20 py-2">
+                    <div className="bg-gradient-to-bl from-orange-300 via-orange-600 to-orange-600 rounded px-4">
+                        <a download href='/Main/Resume-CV.pdf'>Download Resume</a>
+                    </div>
+                    <div className="bg-gradient-to-bl from-orange-300 via-orange-600 to-orange-600 rounded px-4">
+                        Call
+                    </div>
+                </div>
             </div>
         </div>
 
         {/* navigation */}
-        <div className="w-full fixed bottom-0 bg-black bg-opacity-80 flex justify-between z-50">
+        <div className="w-full fixed bottom-0 bg-black bg-opacity-80 flex justify-between z-50 sm:hidden">
             {/* phone icon */}
             <div className="bg-orange-600 bottom-2 left-6 rounded-tr-full w-10 h-20 flex items-center justify-center z-40">
                 <PhoneIphoneIcon />
             </div>
 
             {/* mobile navigation */}
-            <div className='flex flex-col flex-1 relative'>
-                <div className="absolute top-0 h-10 bg-transparent w-full flex items-center justify-center justify-around">
+            <div className='flex flex-col flex-1 relative '>
+                <div className="absolute top-0 h-10 bg-transparent w-full flex items-center justify-around">
                     <Link href='/about'>About</Link>
                     <Link href='/resume'>Resume</Link>
                     <Link href='/work'>Work</Link>
-                    </div>
-                    <div className="absolute bottom-0 h-10 bg-orange-600 w-full flex items-center justify-center">
-                        gg
-                    </div>
-
-
+                </div>
+                <div className="absolute bottom-0 h-10 bg-orange-600 w-full flex items-center justify-center">
+                    <a download href='/Main/Resume-CV.pdf'>Download Resume</a>
+                </div>
                 </div>
 
             {/* email icon */}
