9dfb5558221f25d9609853bf3b6b45179733fc61
James Hyatt <76410700+James-Hyatt-Dev@users.noreply.github.com>
2022-12-01 14:19:25 -0500

    minor additions to Resume page

--- pages/resume.js
+++ pages/resume.js
@@ -9,7 +9,11 @@ import Skills from '../components/Resume/Skills';
 
 function Resume() {
     return (
-        <div className='sm:py-20 pb-16'>
+        <div className='sm:py-10pb-16'>
+            <div className="sticky top-20">
+                <h1 className='text-center text-6xl font-extrabold opacity-5 sm:text-7xl md:text-8xl lg:text-9xl'>Resume</h1>
+            </div>
+
             <div className=" flex flex-col gap-8 md:px-10 lg:max-w-6xl lg:mx-auto lg:border p-8 lg:border-gray-700">
                 <Name />
                 <div className="flex flex-col gap-8 sm:flex-row">
