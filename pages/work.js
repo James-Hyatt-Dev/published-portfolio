76fb29ad569dec572154ad84e6b166a74d54e6b6
James Hyatt <76410700+James-Hyatt-Dev@users.noreply.github.com>
2022-12-01 18:25:09 -0500

    starts the Works page

--- pages/work.js
+++ pages/work.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function work() {
   return (
-    <div>work page</div>
+    <div className='md:w-3/4 md:flex md:flex-col md:mx-auto xl:w-1/2 pb-20'>
+        <h1 className='text-center text-6xl font-extrabold opacity-5 sticky top-20 sm:text-7xl md:text-8xl lg:text-9xl'>Work</h1>
+    </div>
   )
 }
 
