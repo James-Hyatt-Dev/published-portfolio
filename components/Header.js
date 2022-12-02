85c16685e031da0d7878e648b30bf1a48240607b
James Hyatt <76410700+James-Hyatt-Dev@users.noreply.github.com>
2022-12-02 10:57:33 -0500

    add function to email and phone icon mobile nav

--- components/Header.js
+++ components/Header.js
@@ -8,12 +8,12 @@ function Header() {
   return (
     <div className="w-full pb-36 z-50  justify-center">
         {/* Logo */}
-        <div className="flex w-full  py-2 fixed top-0 bg-black bg-opacity-90 items-center z-40">
-            <div className=" w-3/4 flex mx-auto sm:items-center">
-                <div className="flex flex-1 justify-center">
+        <div className="flex w-full  py-2 fixed top-0 bg-black bg-opacity-90 items-center z-40 ">
+            <div className=" w-3/4 flex mx-auto sm:items-center ">
+                <div className="flex flex-1 justify-center lg:justify-start">
                     <Image width={125} src={Logo} alt='james hyatt personal logo containing a single jay and a single h' />
                 </div>
-                <div className="hidden sm:flex sm:items-center gap-20 sm:gap-4  sm:flex-end sm:flex-col">
+                <div className="hidden sm:flex sm:items-center gap-20 sm:gap-4  sm:flex-end sm:flex-col xl:flex-row">
                     <div className="flex justify-center gap-8 sm:gap-8">
                         <Link href='/'>Home</Link>
                         <Link href='/about'>About</Link>
@@ -21,10 +21,10 @@ function Header() {
                         <Link href='/resume'>Resume</Link>
                     </div>
                     <div className="flex gap-2 pr-20 py-2  sm:pr-0 sm:w-full sm:justify-end">
-                        <div className="bg-gradient-to-bl from-orange-300 via-orange-600 to-orange-600 rounded px-4">
+                        <div className="bg-gradient-to-bl from-orange-300 via-orange-600 to-orange-600 rounded px-4 md:px-10 md:py-2">
                             <a download href='/Main/Resume-CV.pdf'>Download Resume</a>
                         </div>
-                        <div className="bg-gradient-to-bl from-orange-300 via-orange-600 to-orange-600 rounded px-4">
+                        <div className="bg-gradient-to-bl from-orange-300 via-orange-600 to-orange-600 rounded px-4 md:px-10 md:py-2">
                             <a href=" tel:12392034777">Call</a>
                         </div>
                     </div>
@@ -36,9 +36,7 @@ function Header() {
         <div className="w-full fixed bottom-0 bg-black bg-opacity-90 flex justify-between z-50 sm:hidden">
             {/* phone icon */}
             <div className="bg-orange-600 bottom-2 left-6 rounded-tr-full w-10 h-20 flex items-center justify-center z-40">
-                <div className="flex items-center justify-center" >
-                    <PhoneIphoneIcon />
-                </div>
+                <a href="tel:12392034777"><div className="flex items-center justify-center" ><PhoneIphoneIcon /></div></a>
             </div>
 
             {/* mobile navigation */}
@@ -56,9 +54,7 @@ function Header() {
 
             {/* email icon */}
             <div className="bg-orange-600 bottom-2 right-6 rounded-tl-full w-10 h-20 flex items-center justify-center z-40">
-                <div className="flex items-center justify-center">
-                    <EmailIcon />
-                </div>
+                <a href="mailto:james@jameshyatt.me"><div className="flex items-center justify-center"><EmailIcon /></div></a>
             </div>
            
         </div>
@@ -67,3 +63,11 @@ function Header() {
 }
 
 export default Header
+
+
+
+
+
+
+
+// call and email buttons mobile nav
